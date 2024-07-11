8323c23558c8c999159be7a2d609efabad7bc02c
leandroleekrap <leandroleekrap@gmail.com>
2024-07-11 18:42:49 -0300

    Teste operadores

--- src/curso/cursoJavaCertification/Testes.java
+++ src/curso/cursoJavaCertification/Testes.java
@@ -4,49 +4,16 @@ import curso.cursoJavaCertification.service.leetcode.OCFACertification1Z0811Serv
 
 class Testes {
 
-	//private ClasseControle classeControle;
 	private EasyDesafioService easyDesafioService;
 	private OCFACertification1Z0811Service oCFACertification1Z0811Service;
 
-    //private EstruturaDadoService estruturaDadoService;
-	//private TestRibeiroPretoService testRibeiroPretoService;
 	public static void main(String[] args){
-		//BancoDeDados bd = new BancoDeDados("localhost", "usuario", "1234");
-		//BalancoEmpresa balanco = new BalancoEmpresa(bd);
-		//ClasseControle.registraDividas(balanco);
-		//ClasseControle.realizaPagamentos(balanco);
-		//ClasseControle.testeThreads();
-		//ClasseControle.acessoHttp();
-		//System.out.println(EasyDesafioService.firstUniqChar("loveleetcode")); // leetcode, loveleetcode, aabb
-		//EstruturaDadoService.lerArquivo();
-		//EstruturaDadoService.resultado_votacao(EstruturaDadoService.vereadores());
-		//EstruturaDadoService.twoSum(new int[]{2, 7, 11, 15}, 9);
-		//TestRibeiroPretoService.valorSoma(13, 0, 0);
-		//TestRibeiroPretoService.fibonacci();
-		//TestRibeiroPretoService.invertString("teste");
-		//System.out.println(args.length);
-		//EasyDesafioService.saidaTesteMaisMais();
-		//System.out.println(EasyDesafioService.verificarTrueFalse());
-		//System.out.println(EasyDesafioService.saidaArrayString());
-		//EasyDesafioService.saidaDivision();
-		///EasyDesafioService.testRandom();
-		//EasyDesafioService.verificarTrueFalseString();
-		//EasyDesafioService.doWhile();
-		//EasyDesafioService.adicionandoRemovendoList();
-		//EasyDesafioService.testandoSaidaRuntimeException();
-		//EasyDesafioService.testesComRemoveClear();
-		//EasyDesafioService.sensitiveCase();
-		//EasyDesafioService.testeCompateTo();
-		//EasyDesafioService.testeTrueAndFalse();
-		//EasyDesafioService.testStringAndChar();
-		//EasyDesafioService.testeArrayList();
-		//EasyDesafioService.testeFor();
-		//EasyDesafioService.testeTrueAndFalseTwo();
-		//EasyDesafioService.testeString();
-		//EasyDesafioService.testeClassParametros();
 		OCFACertification1Z0811Service.somador();
 
-	
+		int a = 2;
+		int b = 5;
+		int c = a * (a++ - --b) * a + b;
+		System.out.println(a+" " + b + " " + c);
 }
 
 }
